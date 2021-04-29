8b25396cc26dd210f24948c8a36a704eca615a5c
manjoe <dibw0216@yahoo.co.jp>
2021-04-29 19:29:56 +0900

    20 アロー関数

--- src/index.js
+++ src/index.js
@@ -54,13 +54,38 @@ console.log(val3);
 /**
  * テンプレート文字列
  */
-const name = "Taro";
-const age = 28;
+// const name = "Taro";
+// const age = 28;
 
-// 私の名前はTaroです。年齢は28歳です
-// 従来の方法
-const message1 = "私の名前は" + name + "です。年齢は" + age + "歳です。";
-console.log(message1);
-// テンプレート文字列を用いた方法
-const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
-console.log(message2);
+// // 私の名前はTaroです。年齢は28歳です
+// // 従来の方法
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "歳です。";
+// console.log(message1);
+// // テンプレート文字列を用いた方法
+// const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+/**
+ * アロー関数
+ */
+// 従来の関数
+// function func1(str) {
+//   return str;
+// }
+const func1 = function (str) {
+  return str;
+};
+console.log(func1("func1です"));
+
+// アロー関数
+// const func2 = (str) => {
+//   return str;
+// };
+// 処理1行でかける場合などはブラケットを省略できる
+const func2 = (str) => str;
+console.log(func2("func2です"));
+
+const func3 = (num1, num2) => {
+  return num1 + num2;
+};
+console.log(func3(10, 20));
