4de5f7466f4bc933958ef93356fd3eb8443b8535
manjoe <dibw0216@yahoo.co.jp>
2021-05-08 02:48:14 +0900

    26 論理演算子の本当の意味 || は左側がfalseとなるとき右側を返す && は左側がtrueなら右側を返す

--- src/index.js
+++ src/index.js
@@ -201,7 +201,30 @@ const nameArr = ["田中", "山田", "じゃけ"];
 //   typeof num === "number" ? num.toLocaleString() : "数値を入力してください";
 // console.log(formattedNum);
 
-const checkSum = (num1, num2) => {
-  return num1 + num2 > 100 ? "100を超えています" : "許容範囲内です。";
-};
-console.log(checkSum(50, 6));
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? "100を超えています" : "許容範囲内です。";
+// };
+// console.log(checkSum(50, 6));
+
+/**
+ * 論理演算子の本当の意味を知ろう && ||
+ */
+// const flag1 = true;
+// const flag2 = false;
+
+// if (flag1 || flag2) {
+//   console.log("1か2はTrueになります");
+// }
+// if (flag1 && flag2) {
+//   console.log("1も2もTrueになります");
+// }
+
+// || は左側がfalseとなるとき右側を返す
+const num = null;
+const fee = num || "金額未設定です";
+console.log(fee);
+
+// && は左側がtrueなら右側を返す
+const num2 = 100;
+const fee2 = num2 && "何か設定されました";
+console.log(fee2);
