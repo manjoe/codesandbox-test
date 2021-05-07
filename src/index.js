a2dc5a536b7f078c7cbe4f374934d2d4cfff0f99
manjoe <dibw0216@yahoo.co.jp>
2021-05-08 02:37:28 +0900

    26 三項演算子

--- src/index.js
+++ src/index.js
@@ -179,11 +179,29 @@ const nameArr = ["田中", "山田", "じゃけ"];
 // })
 // console.log(newNumArr);
 
-const newNameArr = nameArr.map((name) => {
-  if (name === "じゃけ") {
-    return name;
-  } else {
-    return `${name}さん`;
-  }
-});
-console.log(newNameArr);
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "じゃけ") {
+//     return name;
+//   } else {
+//     return `${name}さん`;
+//   }
+// });
+// console.log(newNameArr);
+
+/**
+ * 三項演算子
+ */
+// ある条件 ? 条件がTrueのとき : 条件がFalseのとき
+// const val1 = 1 < 0 ? "trueです" : "falseです";
+// console.log(val1);
+// const num = "1300";
+// console.log(num.toLocaleString());
+
+// const formattedNum =
+//   typeof num === "number" ? num.toLocaleString() : "数値を入力してください";
+// console.log(formattedNum);
+
+const checkSum = (num1, num2) => {
+  return num1 + num2 > 100 ? "100を超えています" : "許容範囲内です。";
+};
+console.log(checkSum(50, 6));
