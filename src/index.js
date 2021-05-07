62ef24ef7f8d3162a4d8047f19ca3a010de752e9
manjoe <dibw0216@yahoo.co.jp>
2021-05-08 01:34:55 +0900

    23 スプレッド構文

--- src/index.js
+++ src/index.js
@@ -117,7 +117,41 @@ console.log(val3);
  * デフォルト値、引数など
  */
 
-const sayHello = (name = "ゲスト") => console.log(`こんにちは!${name}さん!`);
-sayHello("じゃけ"); //こんにちは!じゃけさん!
-sayHello(); //こんにちは!undefinedさん!
-// name = "ゲスト"と初期値を代入することもできる。未指定の場合undefindになり、バグの温床となりやすい
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは!${name}さん!`);
+// sayHello("じゃけ"); //こんにちは!じゃけさん!
+// sayHello(); //こんにちは!undefinedさん!
+// // name = "ゲスト"と初期値を代入することもできる。未指定の場合undefindになり、バグの温床となりやすい
+
+/**
+ * スプレッド構文 ...
+ */
+// 配列の展開
+// const arr1 = [1, 2];
+// // console.log(arr1);
+// // console.log(...arr1);
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+// // まとめる
+// const arr2 = [1,2,3,4,5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);// 1
+// console.log(num2);// 2
+// console.log(arr3);// [3, 4, 5]
+
+// 配列のコピー、結合
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
+// const arr6 = [...arr4];
+// arr6[0] = 100;
+// console.log(arr6);
+// console.log(arr4);
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7); // [10, 20, 30, 40]
+
+// // =でコピーすると参照渡しになる
+// const arr8 = arr4;
+// arr8[0] = 100;
+// console.log(arr8); // [100, 20]
+// console.log(arr4); // [100, 20]
