0cbe6a174ee595c55c9641044138777502431860
manjoe <dibw0216@yahoo.co.jp>
2021-05-08 03:47:59 +0900

    32 タスクの機能追加　テキスト

--- src/index.js
+++ src/index.js
@@ -1,230 +1,253 @@
-/**
- * const, letの変数宣言
- */
-// var val1 = "var変数";
-// console.log(val1);
+import "./styles.css";
 
-// // var変数は上書き可能
-// val1 = "var変数を上書き";
-// console.log(val1);
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+  // li作成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+  // divタグの子要素に各要素を設定
+  div.appendChild(li);
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+  console.log(div);
+};
 
-// // var変数は再宣言可能
-// var val1 = "var変数を再宣言";
-// console.log(val1);
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
 
-// let val2 = "let変数";
-// console.log(val2);
+// /**
+//  * const, letの変数宣言
+//  */
+// // var val1 = "var変数";
+// // console.log(val1);
 
-// // letは上書き可能
-// val2 = "let変数は上書き可能"
-// console.log(val2);
+// // // var変数は上書き可能
+// // val1 = "var変数を上書き";
+// // console.log(val1);
 
-// // letは再宣言不可能
-// let val2 = "let変数を再宣言";
-// // SyntaxError: /src/index.js: Identifier 'val2' has already been declared (23:4)
+// // // var変数は再宣言可能
+// // var val1 = "var変数を再宣言";
+// // console.log(val1);
 
-const val3 = "const変数";
-console.log(val3);
+// // let val2 = "let変数";
+// // console.log(val2);
 
-// const変数は上書き不可能
-// val3 = "const変数を上書き";
-// TypeError: "val3" is read-only
+// // // letは上書き可能
+// // val2 = "let変数は上書き可能"
+// // console.log(val2);
 
-// constは再宣言できない
-// const val3 = "const変数を再宣言";
-// SyntaxError: /src/index.js: Identifier 'val3' has already been declared (34:6)
+// // // letは再宣言不可能
+// // let val2 = "let変数を再宣言";
+// // // SyntaxError: /src/index.js: Identifier 'val2' has already been declared (23:4)
 
-// constで定義したオブジェクトはプロパティの変更が可能
-// const val4 = {
-//   name: "Taro",
-//   age: "28"
-// };
-// console.log(val4);
-// val4.name = "太郎";
-// console.log(val4);
-// val4.address = "Hiroshima";
-// console.log(val4);
+// const val3 = "const変数";
+// console.log(val3);
 
-// constで定義した配列はプロパティの変更が可能
-// const val5 = ["dog", "cat"];
-// val5[0] = "bird";
-// val5.push("monkey");
-// console.log(val5);
+// // const変数は上書き不可能
+// // val3 = "const変数を上書き";
+// // TypeError: "val3" is read-only
 
-/**
- * テンプレート文字列
- */
-// const name = "Taro";
-// const age = 28;
+// // constは再宣言できない
+// // const val3 = "const変数を再宣言";
+// // SyntaxError: /src/index.js: Identifier 'val3' has already been declared (34:6)
 
-// // 私の名前はTaroです。年齢は28歳です
-// // 従来の方法
-// const message1 = "私の名前は" + name + "です。年齢は" + age + "歳です。";
-// console.log(message1);
-// // テンプレート文字列を用いた方法
-// const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
-// console.log(message2);
+// // constで定義したオブジェクトはプロパティの変更が可能
+// // const val4 = {
+// //   name: "Taro",
+// //   age: "28"
+// // };
+// // console.log(val4);
+// // val4.name = "太郎";
+// // console.log(val4);
+// // val4.address = "Hiroshima";
+// // console.log(val4);
 
-/**
- * アロー関数
- */
-// 従来の関数
-// function func1(str) {
-//   return str;
-// }
-// const func1 = function (str) {
-//   return str;
-// };
-// console.log(func1("func1です"));
+// // constで定義した配列はプロパティの変更が可能
+// // const val5 = ["dog", "cat"];
+// // val5[0] = "bird";
+// // val5.push("monkey");
+// // console.log(val5);
 
-// アロー関数
-// const func2 = (str) => {
-//   return str;
-// };
-// 処理1行でかける場合などはブラケットを省略できる
-// const func2 = (str) => str;
-// console.log(func2("func2です"));
+// /**
+//  * テンプレート文字列
+//  */
+// // const name = "Taro";
+// // const age = 28;
 
-// const func3 = (num1, num2) => {
-//   return num1 + num2;
-// };
-// console.log(func3(10, 20));
+// // // 私の名前はTaroです。年齢は28歳です
+// // // 従来の方法
+// // const message1 = "私の名前は" + name + "です。年齢は" + age + "歳です。";
+// // console.log(message1);
+// // // テンプレート文字列を用いた方法
+// // const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
+// // console.log(message2);
 
-/**
- * 分割代入　ES2015から　知らないと読めないコード
- */
-// const myProfile = {
-//   name: "じゃけ",
-//   age: 28
-// };
+// /**
+//  * アロー関数
+//  */
+// // 従来の関数
+// // function func1(str) {
+// //   return str;
+// // }
+// // const func1 = function (str) {
+// //   return str;
+// // };
+// // console.log(func1("func1です"));
 
-// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}です。`;
-// console.log(message1);
-// // 変数名が長いと冗長
+// // アロー関数
+// // const func2 = (str) => {
+// //   return str;
+// // };
+// // 処理1行でかける場合などはブラケットを省略できる
+// // const func2 = (str) => str;
+// // console.log(func2("func2です"));
 
-// const { name, age } = myProfile;
-// const message2 = `名前は${name}です。年齢は${age}です。`;
+// // const func3 = (num1, num2) => {
+// //   return num1 + num2;
+// // };
+// // console.log(func3(10, 20));
 
-// const myProfile = ["じゃけ", 28];
-// const message3 = `名前は${myProfile[0]}です。年連は${myProfile[1]}です。`;
-// console.log(message3);
-// // 順番に取り出す
-// const [name, age] = myProfile;
-// const message4 = `名前は${name}です。年齢は${age}です。`;
-// console.log(message4);
+// /**
+//  * 分割代入　ES2015から　知らないと読めないコード
+//  */
+// // const myProfile = {
+// //   name: "じゃけ",
+// //   age: 28
+// // };
 
-/**
- * デフォルト値、引数など
- */
+// // const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}です。`;
+// // console.log(message1);
+// // // 変数名が長いと冗長
 
-// const sayHello = (name = "ゲスト") => console.log(`こんにちは!${name}さん!`);
-// sayHello("じゃけ"); //こんにちは!じゃけさん!
-// sayHello(); //こんにちは!undefinedさん!
-// // name = "ゲスト"と初期値を代入することもできる。未指定の場合undefindになり、バグの温床となりやすい
+// // const { name, age } = myProfile;
+// // const message2 = `名前は${name}です。年齢は${age}です。`;
 
-/**
- * スプレッド構文 ...
- */
-// 配列の展開
-// const arr1 = [1, 2];
-// // console.log(arr1);
-// // console.log(...arr1);
-// const sumFunc = (num1, num2) => console.log(num1 + num2);
-// sumFunc(arr1[0], arr1[1]);
-// sumFunc(...arr1);
+// // const myProfile = ["じゃけ", 28];
+// // const message3 = `名前は${myProfile[0]}です。年連は${myProfile[1]}です。`;
+// // console.log(message3);
+// // // 順番に取り出す
+// // const [name, age] = myProfile;
+// // const message4 = `名前は${name}です。年齢は${age}です。`;
+// // console.log(message4);
 
-// // まとめる
-// const arr2 = [1,2,3,4,5];
-// const [num1, num2, ...arr3] = arr2;
-// console.log(num1);// 1
-// console.log(num2);// 2
-// console.log(arr3);// [3, 4, 5]
+// /**
+//  * デフォルト値、引数など
+//  */
 
-// 配列のコピー、結合
-// const arr4 = [10, 20];
-// const arr5 = [30, 40];
-// const arr6 = [...arr4];
-// arr6[0] = 100;
-// console.log(arr6);
-// console.log(arr4);
-// const arr7 = [...arr4, ...arr5];
-// console.log(arr7); // [10, 20, 30, 40]
+// // const sayHello = (name = "ゲスト") => console.log(`こんにちは!${name}さん!`);
+// // sayHello("じゃけ"); //こんにちは!じゃけさん!
+// // sayHello(); //こんにちは!undefinedさん!
+// // // name = "ゲスト"と初期値を代入することもできる。未指定の場合undefindになり、バグの温床となりやすい
 
-// // =でコピーすると参照渡しになる
-// const arr8 = arr4;
-// arr8[0] = 100;
-// console.log(arr8); // [100, 20]
-// console.log(arr4); // [100, 20]
+// /**
+//  * スプレッド構文 ...
+//  */
+// // 配列の展開
+// // const arr1 = [1, 2];
+// // // console.log(arr1);
+// // // console.log(...arr1);
+// // const sumFunc = (num1, num2) => console.log(num1 + num2);
+// // sumFunc(arr1[0], arr1[1]);
+// // sumFunc(...arr1);
 
-/**
- * mapやfilterを使った配列の処理
- */
+// // // まとめる
+// // const arr2 = [1,2,3,4,5];
+// // const [num1, num2, ...arr3] = arr2;
+// // console.log(num1);// 1
+// // console.log(num2);// 2
+// // console.log(arr3);// [3, 4, 5]
 
-const nameArr = ["田中", "山田", "じゃけ"];
-// for (let index = 0; index < nameArr.length; index++){
-//   console.log(`${index + 1}番目は${nameArr[index]}です。`);
-// }
+// // 配列のコピー、結合
+// // const arr4 = [10, 20];
+// // const arr5 = [30, 40];
+// // const arr6 = [...arr4];
+// // arr6[0] = 100;
+// // console.log(arr6);
+// // console.log(arr4);
+// // const arr7 = [...arr4, ...arr5];
+// // console.log(arr7); // [10, 20, 30, 40]
 
-// const nameArr2 = nameArr.map((name)=>{
-//   return name;
-// })
-// console.log(nameArr2);
+// // // =でコピーすると参照渡しになる
+// // const arr8 = arr4;
+// // arr8[0] = 100;
+// // console.log(arr8); // [100, 20]
+// // console.log(arr4); // [100, 20]
 
-// 配列の場合、2個目の引数はindexになるので必要に応じて利用する
-// nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です。`));
+// /**
+//  * mapやfilterを使った配列の処理
+//  */
 
-// const numArr = [1, 2, 3, 4, 5];
-// const newNumArr = numArr.filter((num)=>{
-//   return num % 2 === 1;
-// })
-// console.log(newNumArr);
+// const nameArr = ["田中", "山田", "じゃけ"];
+// // for (let index = 0; index < nameArr.length; index++){
+// //   console.log(`${index + 1}番目は${nameArr[index]}です。`);
+// // }
 
-// const newNameArr = nameArr.map((name) => {
-//   if (name === "じゃけ") {
-//     return name;
-//   } else {
-//     return `${name}さん`;
-//   }
-// });
-// console.log(newNameArr);
+// // const nameArr2 = nameArr.map((name)=>{
+// //   return name;
+// // })
+// // console.log(nameArr2);
 
-/**
- * 三項演算子
- */
-// ある条件 ? 条件がTrueのとき : 条件がFalseのとき
-// const val1 = 1 < 0 ? "trueです" : "falseです";
-// console.log(val1);
-// const num = "1300";
-// console.log(num.toLocaleString());
+// // 配列の場合、2個目の引数はindexになるので必要に応じて利用する
+// // nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です。`));
 
-// const formattedNum =
-//   typeof num === "number" ? num.toLocaleString() : "数値を入力してください";
-// console.log(formattedNum);
+// // const numArr = [1, 2, 3, 4, 5];
+// // const newNumArr = numArr.filter((num)=>{
+// //   return num % 2 === 1;
+// // })
+// // console.log(newNumArr);
 
-// const checkSum = (num1, num2) => {
-//   return num1 + num2 > 100 ? "100を超えています" : "許容範囲内です。";
-// };
-// console.log(checkSum(50, 6));
+// // const newNameArr = nameArr.map((name) => {
+// //   if (name === "じゃけ") {
+// //     return name;
+// //   } else {
+// //     return `${name}さん`;
+// //   }
+// // });
+// // console.log(newNameArr);
 
-/**
- * 論理演算子の本当の意味を知ろう && ||
- */
-// const flag1 = true;
-// const flag2 = false;
+// /**
+//  * 三項演算子
+//  */
+// // ある条件 ? 条件がTrueのとき : 条件がFalseのとき
+// // const val1 = 1 < 0 ? "trueです" : "falseです";
+// // console.log(val1);
+// // const num = "1300";
+// // console.log(num.toLocaleString());
 
-// if (flag1 || flag2) {
-//   console.log("1か2はTrueになります");
-// }
-// if (flag1 && flag2) {
-//   console.log("1も2もTrueになります");
-// }
+// // const formattedNum =
+// //   typeof num === "number" ? num.toLocaleString() : "数値を入力してください";
+// // console.log(formattedNum);
 
-// // || は左側がfalseとなるとき右側を返す
-// const num = null;
-// const fee = num || "金額未設定です";
-// console.log(fee);
+// // const checkSum = (num1, num2) => {
+// //   return num1 + num2 > 100 ? "100を超えています" : "許容範囲内です。";
+// // };
+// // console.log(checkSum(50, 6));
 
-// // && は左側がtrueなら右側を返す
-// const num2 = 100;
-// const fee2 = num2 && "何か設定されました";
-// console.log(fee2);
+// /**
+//  * 論理演算子の本当の意味を知ろう && ||
+//  */
+// // const flag1 = true;
+// // const flag2 = false;
+
+// // if (flag1 || flag2) {
+// //   console.log("1か2はTrueになります");
+// // }
+// // if (flag1 && flag2) {
+// //   console.log("1も2もTrueになります");
+// // }
+
+// // // || は左側がfalseとなるとき右側を返す
+// // const num = null;
+// // const fee = num || "金額未設定です";
+// // console.log(fee);
+
+// // // && は左側がtrueなら右側を返す
+// // const num2 = 100;
+// // const fee2 = num2 && "何か設定されました";
+// // console.log(fee2);
